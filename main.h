0902cf427163ae7c80c6496418baaecb700d3c9c
Asheryram <ashertettehabotsi@gmail.com>
2023-10-17 16:36:49 +0000

    updated1

--- main.h
+++ main.h
@@ -20,7 +20,7 @@
 
 /**
  * struct fmt - Struct op
- * by Yram and Cleeric
+ *
  * @fmt: The format.
  * @fn: The function associated.
  */
@@ -115,4 +115,3 @@ long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
 
 #endif /* MAIN_H */
-
